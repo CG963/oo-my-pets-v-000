259be921d0b5e8ce8a91ce459ea679b2979b3cb2
Christopher Garcia <christopher.garcia08@gmail.com>
2021-03-22 00:15:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,12 @@
-require 'pry'
+require "pry"
 
 class Owner
+
   attr_reader :name, :species
-  
   @@all = []
-  
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
   end
 
   def species(species = "human")
@@ -17,51 +16,50 @@ class Owner
   def say_species
     "I am a #{species}."
   end
-  
-  def self.all 
+
+  def self.all
     @@all
   end
 
   def self.count
-    self.all.count
+    @@all.count
   end
-  
+
   def self.reset_all
-    self.all.clear
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select { |name| name.owner == self }
   end
   
-  def dogs 
-    Dog.all.select{|name| name.owner == self}
+  def dogs
+    Dog.all.select { |name| name.owner == self }
   end
-  
-  def cats 
-    Cat.all.select{|name| name.owner == self}
+
+  def buy_cat(cat)
+    new_cat = Cat.new(cat, self)
   end
   
   def buy_dog(dog)
     new_dog = Dog.new(dog, self)
   end
 
-  def buy_cat(cat)
-    new_cat = Cat.new(cat, self) 
-  end
-
   def walk_dogs
-    Dog.all.each {|dog| dog.mood = "happy"}
+    Dog.all.each {|x| x.mood = "happy"}
   end
 
   def feed_cats
-    Cat.all.each {|cat| cat.mood = "happy"}
+    Cat.all.each {|x| x.mood = "happy"}
   end
 
   def sell_pets
-    pets = self.dogs + self.cats
-    
-    pets.each do |pet|
-      pet.mood = "nervous"
-      pet.owner = nil 
+    Dog.all.each {|x| x.mood = "nervous"}
+    Cat.all.each {|x| x.mood = "nervous"}
+    Dog.all.each {|x| x.owner = nil}
+    Cat.all.each {|x| x.owner = nil}
   end
-  
+
   def list_pets
     "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
   end
